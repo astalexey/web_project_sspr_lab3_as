42686388c556f9b61cb36474172e7997e8ebda4f
Alexey Astafev <astafev1502276@bk.ru>
2022-11-29 10:49:33 +0400

    Initial commit

---
+++ src/main/java/Functions.java
@@ -0,0 +1,17 @@
+public class Functions {
+    public static int sum (int a, int b){
+        return a + b;
+    }
+
+    public static int sub (int a, int b){
+        return a - b;
+    }
+
+    public static int mul (int a, int b){
+        return a * b;
+    }
+
+    public static double div (int a, int b) {
+        return (double) (a / b);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Functions.sum(15,5));
+        System.out.println(Functions.sub(15,5));
+        System.out.println(Functions.mul(15,5));
+        System.out.println(Functions.div(15,5));
+    }
+}
