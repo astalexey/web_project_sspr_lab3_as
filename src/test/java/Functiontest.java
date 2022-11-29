a3f020aad24a0d2b4cf7ed0799c6d41ae58d566c
Alexey Astafev <astafev1502276@bk.ru>
2022-11-29 11:36:09 +0400

    com2

---
+++ src/test/java/Functiontest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Functiontest {
+    @Test
+    public void tesIsFive() {
+        int i = Functions.sum(6, 5);
+        Assertions.assertEquals(11, i);
+    }
+    @Test
+    public void tesPow() {
+        int i = Functions.sub(6, 5);
+        Assertions.assertEquals(1, i);
+    }
+    @Test
+    public void testConcat() {
+        int i = Functions.mul(6, 5);
+        Assertions.assertEquals(30, i);
+    }
+}
