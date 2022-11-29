b108d690dedb2a429f027e97ab5171e6943a9291
Alexey Astafev <astafev1502276@bk.ru>
2022-11-29 11:27:05 +0400

    com2

---
+++ src/test/java/web_project_sspr_lab3_as/SsprTestLab.java
@@ -0,0 +1,28 @@
+package web_project_sspr_lab3_as;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SsprTestLab {
+    @Test
+    public void test(){
+        int x = 2;
+        int y = 23;
+        assertEquals(46, x*y);
+    }
+
+    @Test
+    public void test2(){
+        int x = 3;
+        int y = 23;
+        assertEquals(26, x+y);
+    }
+
+    @Test
+    public void test3(){
+        int x = 44;
+        int y = 11;
+        assertEquals(4, x/y);
+    }
+}
